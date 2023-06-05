8b686b647bbe30725914a37250b9730b399d865b
MariaCode <m.p.arnaudova@gmail.com>
2023-06-05 22:47:00 +0300

    Text Splitter App

---
+++ CSharpWebFund-MVCIntro-Exercise/TextSplitter/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿
+namespace TextSplitter.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using TextSplitter.ViewModels;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextSplitViewModel textViewModel)
+        {
+            return View(textViewModel);
+        }
+
+        public IActionResult Split(TextSplitViewModel textViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", textViewModel);   
+            }
+
+            string[] words = textViewModel.TextToSplit
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string splitText  = String.Join(Environment.NewLine, words);    
+
+            textViewModel.SplitText= splitText;
+
+            return RedirectToAction("Index", textViewModel);
+        }
+
+
+    }
+}
