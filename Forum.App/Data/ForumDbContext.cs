943a78f444f093f7811d7bd5267c1a1f0c857511
MariaCode <m.p.arnaudova@gmail.com>
2023-06-07 16:56:33 +0300

    Forum.App created.

---
+++ Forum.App/Data/ForumDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Forum.App.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    public class ForumDbContext : DbContext;
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+            :base(options) 
+        {
+
+        }
+    }
+}
